a714762db18301bb0ac471349d9dd5e54603aca8
krylofficiall <68692106+krylofficiall@users.noreply.github.com>
2022-01-29 11:41:06 +0300

    Update "Clicks by Spacebar"

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let clicks = 0;
-
+console.log("Hello to developers!")
 function addClick() {
     update(++clicks);
+}
+
+document.body.onkeyup = function(key){
+    if(key.keyCode == 32){
+        addClick();
+    }
+    if(key.keyCode == 82){
+        resetClick();
+    }
 }
 
 function resetClick() {
